f54920455546cb213c8958d1bbd458cc79b11ca3
talfig <talfig8@gmail.com>
2024-09-28 15:21:51 +0300

    Deploy static version of Next.js app to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',  // Export the app as static HTML files
+    basePath: '/portfolio',  // Add the GitHub Pages subdirectory
+    assetPrefix: '/portfolio/',  // Prefix for static assets
+    images: {
+      unoptimized: true,  // Since Next.js images won't be optimized on GitHub Pages
+    },
+  };
+  
+  export default nextConfig;
+  
